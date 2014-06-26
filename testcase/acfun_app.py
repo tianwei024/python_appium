6ebeb674bd6af5ee9b949b0c638e6dc9d234687a
tianwei <tianwei024@gmail.com>
2014-06-26 14:14:21 +0800

    更新用例

--- testcase/acfun_app.py
+++ testcase/acfun_app.py
@@ -105,6 +105,139 @@ class Acfun(unittest.TestCase):
         el = self.driver.find_element_by_id('tv.acfundanmaku.video:id/weekly_popup')
         self.assertIsNotNone(el)
 
+        def test_random_click(self):
+        u'''app娱乐随机点击用例'''
+        homepage.yl_random_click(self.driver)
+        el = self.driver.find_element_by_id('tv.acfundanmaku.video:id/content_video_info_stows_add')
+        self.assertIsNotNone(el)
+
+    def test_sc(self):
+        u'''app收藏测试用例'''
+        #登陆
+        public.login(self.driver)
+        #返回到频道
+        homepage.pd_click(self.driver)
+        #随机点击娱乐栏目
+        homepage.yl_random_click(self.driver)
+        #点击收藏
+        homepage.play_sc(self.driver).click()
+        #查找以已收藏元素
+        el = self.driver.find_element_by_id('tv.acfundanmaku.video:id/content_video_info_stows_remove')
+        self.assertIsNotNone(el)
+
+
+
+
+    def test_pl(self):
+        u'''app评论测试用例'''
+        #登陆
+        public.login(self.driver)
+        #返回到频道
+        homepage.pd_click(self.driver)
+        #随机点击娱乐栏目
+        homepage.yl_random_click(self.driver)
+        #点击评论进入评论界面
+        homepage.play_pl(self.driver).click()
+        #点击右上角评论按钮发布评论
+        self.driver.find_element_by_name('评论').click()
+        #输入评论内容
+        el = self.driver.find_element_by_id('tv.acfundanmaku.video:id/post_input')
+        el.send_keys('thanks ok!')
+        #点击发送
+        self.driver.find_element_by_name(u'发送').click()
+        #等页面自动跳转
+        sleep(2)
+        #断言 评论跳转后页面的评论元素是否为空
+        self.assertIsNotNone('homepage.pl_pl(self.driver)')
+
+
+    #有元素定位不到，后面在完成用例
+    def test_px(self):
+        u'''app排序测试用例'''
+        #点击娱乐栏目查看更多
+        homepage.yls_click(self.driver)
+        #点击右上角排序
+        homepage.px(self.driver)
+
+    #文章收藏
+    def test_wzsc(self):
+        u'''app文章收藏测试用例'''
+        #登陆
+        public.login(self.driver)
+        #返回到排行
+        homepage.ph_click(self.driver)
+        #点击排行按钮
+        self.driver.find_element_by_id('tv.acfundanmaku.video:id/rank_popup').click()
+        sleep(2)
+        #选文章排行
+        el = self.driver.find_elements_by_class_name('android.widget.LinearLayout')
+        el[8].click()
+        sleep(2)
+        #点击文章
+        ele = self.driver.find_elements_by_class_name('android.widget.LinearLayout')
+        ele[0].click()
+        #收藏
+        homepage.wz_sc(self.driver).click()
+        sleep(2)
+        # #断言 收藏成功判断已收藏元素是否为空
+        el = self.driver.find_element_by_name(u'取消收藏')
+        self.assertIsNotNone(el)
+
+
+
+    def test_wzpl(self):
+        u'''app文章评论测试用例'''
+        #登陆
+        public.login(self.driver)
+        #返回到排行
+        homepage.ph_click(self.driver)
+        #点击排行按钮
+        self.driver.find_element_by_id('tv.acfundanmaku.video:id/rank_popup').click()
+        sleep(2)
+        #选文章排行
+        el = self.driver.find_elements_by_class_name('android.widget.LinearLayout')
+        el[8].click()
+        sleep(2)
+        #点击文章
+        ele = self.driver.find_elements_by_class_name('android.widget.LinearLayout')
+        ele[0].click()
+        #文章评论
+        homepage.wz_pl(self.driver).click()
+        #点击评论
+
+        self.driver.find_element_by_name(u'评论').click()
+        #输入评论
+        self.driver.find_element_by_id('tv.acfundanmaku.video:id/post_input').send_keys('thinks ok!')
+        self.driver.find_element_by_name(u'发送').click()
+        sleep(2)
+        el = self.driver.find_element_by_name(u'评论')
+        self.assertIsNotNone(el)
+
+
+    def test_wzmhms(self):
+        u'''app文章漫画模式测试用例'''
+        #登陆
+        public.login(self.driver)
+        #返回到排行
+        homepage.ph_click(self.driver)
+        #点击排行按钮
+        self.driver.find_element_by_id('tv.acfundanmaku.video:id/rank_popup').click()
+        sleep(2)
+        #选文章排行
+        el = self.driver.find_elements_by_class_name('android.widget.LinearLayout')
+        el[8].click()
+        sleep(2)
+        #点击文章
+        ele = self.driver.find_elements_by_class_name('android.widget.LinearLayout')
+        ele[0].click()
+        #点击漫画模式
+        homepage.wz_mhms(self.driver).click()
+        sleep(2)
+        #断言 判断漫画模式变为文章模式按键是否为空
+        el = self.driver.find_element_by_id('tv.acfundanmaku.video:id/content_article_info_comic_articlemode')
+        self.assertIsNotNone(el)
+
+
 if __name__ == '__main__':
     unittest.main()
 
